3c48d04629935e6d15ab1d8656ac8930469456a7
oli <oli.loades@gmail.com>
2018-07-11 11:18:30 +0100

    addition feature complete

--- src/test/java/com/qa/calculator/CalculatorTest.java
+++ src/test/java/com/qa/calculator/CalculatorTest.java
@@ -8,8 +8,31 @@ public class CalculatorTest {
 	@Test
 	public void test() {
 		Calculator calc = new Calculator();
+		
 		int actualValue = calc.add(2, 2);
 		Assert.assertEquals(4, actualValue);
 	}
-
+	
+	@Test
+	public void testSub() {
+		Calculator calc = new Calculator();
+	
+		int actualValue = calc.sub(4,2);
+		Assert.assertEquals(2,actualValue);
+	}
+	@Test
+	public void testMulti() {
+		Calculator calc = new Calculator();
+		
+		int actualValue = calc.multiply(4,2);
+		Assert.assertEquals(8,actualValue);
+	}
+	
+	@Test
+	public void testDiv() {
+		Calculator calc = new Calculator();
+	
+		int actualValue = calc.div(4,2);
+		Assert.assertEquals(2,actualValue);
+	}
 }
