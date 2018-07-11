048060b47acfb74c4339092b4e77df4fb54b4a3b
Kate <e.genova@live.co.uk>
2018-07-11 11:23:29 +0100

    subtract green

--- src/test/java/com/qa/calculator/SubtractTest.java
+++ src/test/java/com/qa/calculator/SubtractTest.java
@@ -14,10 +14,11 @@ public class SubtractTest {
 	@Test
 	public void test() {
 		Subtract sub = new Subtract();
-		assertEquals(5, sub.subtract(10-5);
-		assertEquals(2, sub.subtract(8-6));
-		assertEquals(-5, sub.subtract(0-5));
-		assertEquals(-12, sub.subtract(-18-6));
+		assertEquals(5, sub.subtract(10.0,5.0), 0.001);
+		assertEquals(2, sub.subtract(8.0,6.0), 0.001);
+		assertEquals(-5, sub.subtract(0,5.0), 0.001);
+		assertEquals(-24, sub.subtract(-18.0,6.0), 0.001);
+		assertEquals(4.8, sub.subtract(6.2, 1.4),0.001);
 	}
 
 }
