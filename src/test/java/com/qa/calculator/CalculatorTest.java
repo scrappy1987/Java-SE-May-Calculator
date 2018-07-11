85a193bc9e5a8abf1e936197f1a00361964f8ba8
Syed Ahmed <syed9569a@gmail.com>
2018-07-11 12:15:32 +0100

    Added all the basic features of a calculator

--- src/test/java/com/qa/calculator/CalculatorTest.java
+++ src/test/java/com/qa/calculator/CalculatorTest.java
@@ -6,27 +6,48 @@ import org.junit.Test;
 
 public class CalculatorTest {
 	
-	private Calculator calc;
+	private Addition addition;
+	private Substraction substract;
+	private Multiplication  multiply;
+	private Division division;
 	
 	@Before
 	public void setUp() {
 		
-		calc = new Calculator();
+		addition = new Addition();
+		substract = new Substraction();
+		multiply = new Multiplication();
+		division = new Division();
 		
 	}
 
 	@Test
-	public void test() {
-		Calculator calc = new Calculator();
-		int actualValue = calc.add(2, 2);
+	public void tesAddition() {
+		int actualValue = addition.add(2, 2);
 		Assert.assertEquals(4, actualValue);
 	}
 	
 	@Test
-	public void testSubtraction() {
+	public void testMultiplication() {
 		
-		int substractAnswer = calc.substract(4, 2);
-		Assert.assertEquals(2, substractAnswer);
+		int multiplicationAnswer = multiply.multiplication(4, 2);
+		Assert.assertEquals(8, multiplicationAnswer);
+		
+	}
+	
+	@Test
+	public void testSubstract() {
+		
+		int substractAnswer = substract.substract(6, 2);
+		Assert.assertEquals(4,  substractAnswer);
+		
+	}
+	
+	@Test
+	public void testDivision() {
+		
+		int divisionAnswer = division.division(8, 4);
+		Assert.assertEquals(2, divisionAnswer);
 		
 	}
 
