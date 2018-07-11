5e24fa14a707922cc4c585f17ced4612bee9ef53
Benny <siunamwongbenny@gmail.com>
2018-07-11 11:03:30 +0100

    Minus added and tests works

--- src/main/java/com/qa/calculator/Calculator.java
+++ src/main/java/com/qa/calculator/Calculator.java
@@ -6,4 +6,9 @@ public class Calculator {
 		return num1 + num2;
 	}
 
+	public int minus(int i, int j) {
+		
+		return i-j;
+	}
+
 }
--- src/test/java/com/qa/calculator/CalculatorTest.java
+++ src/test/java/com/qa/calculator/CalculatorTest.java
@@ -4,12 +4,21 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CalculatorTest {
-
+	
+	private static Calculator calc = new Calculator();
+	
 	@Test
-	public void test() {
-		Calculator calc = new Calculator();
+	public void add() {
+		
 		int actualValue = calc.add(2, 2);
 		Assert.assertEquals(4, actualValue);
 	}
+	
+	@Test
+	public void minus() {
+		int actualValue = calc.minus(6, 3);
+		Assert.assertEquals(3, actualValue);
+	}
+	
 
 }
